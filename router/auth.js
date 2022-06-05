e2c5eb9687eb066d48e503a9bb32accbab42660a
AndKurt <kurtenkov.as@gmail.com>
2022-06-05 14:37:20 +0300

    fix: headers field authorization

--- router/auth.js
+++ router/auth.js
@@ -65,7 +65,7 @@ router.post('/login', async (req, res) => {
 
   // Create and assign token
   const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
-  res.header('auth-token', token).send(token);
+  res.header('Authorization', token).send(token);
 });
 
 module.exports = router;
